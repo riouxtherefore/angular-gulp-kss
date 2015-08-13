1ffb95452eaeed5a73b24ecf22591aa25de66008
Sean <sean@therefore.ca>
2015-08-13 16:32:24 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # angular-styleguide
-An angular web app with kss style guide generator
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.12.1.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularStyleguideApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularStyleguideApp
+ */
+angular.module('angularStyleguideApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
